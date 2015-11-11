9329b503e61e5e43fa1fd6bbc7f29952a8230afb
Vasiliy Tolstov <v.tolstov@selfip.ru>
2015-11-11 11:56:15 +0000

    install curl
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- scripts/base.sh
+++ scripts/base.sh
@@ -1,4 +1,7 @@
 #!/bin/sh -x
 
-
-pkg_add curl
+ftp ftp://ftp.NetBSD.org/pub/pkgsrc/current/pkgsrc.tar.gz
+tar -xzf pkgsrc.tar.gz -C /usr
+rm -f pkgsrc.tar.gz
+cd /usr/pkgsrc/www/curl
+make install clean clean-depends distclean
